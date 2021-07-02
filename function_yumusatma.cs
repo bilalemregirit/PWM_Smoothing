b8d0b76d0ae4e7133606ca8f1c566fc888a563d3
Bilal Emre Girit <60060887+bilalemregirit@users.noreply.github.com>
2021-07-02 17:40:03 +0300

    Update function_yumusatma.cs

--- function_yumusatma.cs
+++ function_yumusatma.cs
@@ -3,74 +3,79 @@
   int analog_yumusatilmis;
   int hassasiyet deger = 2;
   int olubolge_deger = 1;
+  int index;
   
-  
-  analog_yumusatilmis= yumusatma(analog_input, hassasiyet_deger, olubolge_deger);
-  
-  
-  
-  public int yeni_deger;
-        public int yumusatma(int deger, int hassasiyet, int olu_bolge)
-        {  
-            // Bu fonksiyonun kullanımı için giriş için ve 
+  //ÖRNEK
+  analog_yumusatilmis= yumusatma(analog_input, hassasiyet_deger, olubolge_deger, index);
+   
+             // Bu fonksiyonun kullanımı için giriş için ve 
              //çıkış sinyalleri için 2 ayrı değişken kullanmalısınız 
              //ve fonksiyonun çağırıldığı yer tekrar içinde bulunmalı.
-
              //deger ----------- giris sinyali(yumuşatılmak istenen)
              //hassasiyet ------ sinyalin anlık arttırlıp azaltılışı
-                             //(0dan büyük bütün int degerlerini alabilir)
+             //(0dan büyük bütün int degerlerini alabilir)
              //olu_bolge -------- sinyalin tepki aralığı (0,1,2,3,4 düzeyi arttırmak için)
+             // her bir yumusatılan sinyal için ayrı index degeri veriniz.
 
-                if (deger > yeni_deger)
-                {for (int i = 0; i < 1; i++)
-                {yeni_deger = yeni_deger + hassasiyet;}}  
-                                 
-                if (deger < yeni_deger)
-                {for (int i = 0; i < 1; i++)
-                {yeni_deger = yeni_deger - hassasiyet;}    
-                }
+public int[] degisen_deger = new int[20];
+        private int yumusatma(int deger, int hassasiyet, int olu_bolge, int yeni_deger)
+        {
+            int z = yeni_deger;
+
+            if (deger > degisen_deger[z])
+            {
+                for (int i = 0; i < 1; i++)
+                { degisen_deger[z] = degisen_deger[z] + hassasiyet; }
+            }
+
+            if (deger < degisen_deger[z])
+            {
+                for (int i = 0; i < 1; i++)
+                { degisen_deger[z] = degisen_deger[z] - hassasiyet; }
+            }
             switch (olu_bolge)
             {
-                case 0 :
-                    if (deger == yeni_deger)
-                    {  }
+                case 0:
+                    if (deger == degisen_deger[z])
+                    { }
                     break;
                 case 1:
-                    if (deger == yeni_deger ||
-                        deger == yeni_deger + 1 ||
-                        deger == yeni_deger - 1)
-                    {  }
+                    if (deger == degisen_deger[z] ||
+                        deger == degisen_deger[z] + 1 ||
+                        deger == degisen_deger[z] - 1)
+                    { }
                     break;
                 case 2:
-                    if (deger == yeni_deger ||
-                      deger == yeni_deger + 1 ||
-                      deger == yeni_deger - 1 ||
-                      deger == yeni_deger + 2 ||
-                      deger == yeni_deger - 2)
+                    if (deger == degisen_deger[z] ||
+                      deger == degisen_deger[z] + 1 ||
+                      deger == degisen_deger[z] - 1 ||
+                      deger == degisen_deger[z] + 2 ||
+                      deger == degisen_deger[z] - 2)
                     { }
                     break;
                 case 3:
-                    if (deger == yeni_deger ||
-                      deger == yeni_deger + 1 ||
-                      deger == yeni_deger - 1 ||
-                      deger == yeni_deger + 2 ||
-                      deger == yeni_deger - 2 ||
-                      deger == yeni_deger + 3 ||
-                      deger == yeni_deger - 3)
-                    {  }
+                    if (deger == degisen_deger[z] ||
+                      deger == degisen_deger[z] + 1 ||
+                      deger == degisen_deger[z] - 1 ||
+                      deger == degisen_deger[z] + 2 ||
+                      deger == degisen_deger[z] - 2 ||
+                      deger == degisen_deger[z] + 3 ||
+                      deger == degisen_deger[z] - 3)
+                    { }
                     break;
                 case 4:
-                    if (deger == yeni_deger ||
-                      deger == yeni_deger + 1 ||
-                      deger == yeni_deger - 1 ||
-                      deger == yeni_deger + 2 ||
-                      deger == yeni_deger - 2 ||
-                      deger == yeni_deger + 3 ||
-                      deger == yeni_deger - 3 ||
-                      deger == yeni_deger + 4 ||
-                      deger == yeni_deger - 4)
+                    if (deger == degisen_deger[z] ||
+                      deger == degisen_deger[z] + 1 ||
+                      deger == degisen_deger[z] - 1 ||
+                      deger == degisen_deger[z] + 2 ||
+                      deger == degisen_deger[z] - 2 ||
+                      deger == degisen_deger[z] + 3 ||
+                      deger == degisen_deger[z] - 3 ||
+                      deger == degisen_deger[z] + 4 ||
+                      deger == degisen_deger[z] - 4)
                     { }
                     break;
             }
-            return yeni_deger;
+            return degisen_deger[z];
         }
+
